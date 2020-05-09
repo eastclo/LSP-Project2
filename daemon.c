746096536d46a23436445b94d0b43dd8c0d754a7
eastclo <goldeastcloud@gmail.com>
2020-05-09 20:42:03 +0900

    모니터링 프로그램 구현완료

--- daemon.c
+++ daemon.c
@@ -5,8 +5,11 @@
 #include <signal.h>
 #include <syslog.h>
 #include <string.h>
+#include <dirent.h>
+#include <time.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/time.h>
 #include "daemon.h"
 
 int main(void)
@@ -22,17 +25,24 @@ int main(void)
 	strcat(mntrDir, "/check");
 	strcat(logFile, "/log.txt");
 
-	if(daemon_init() < 0) {
-		fprintf(stderr, "daemon process isn't created\n");
-		exit(1);
-	}
+	//	if(daemon_init() < 0) {
+	//		fprintf(stderr, "daemon process isn't created\n");
+	//		exit(1);
+	//	}
 
-	//log.txt파일 오픈
+	fp = fopen(logFile, "w+"); //log.txt파일 오픈
+	setbuf(fp, NULL);
+
+	Llist* list = (Llist*)malloc(sizeof(Llist)); //모니터링시 파일과 수정시간을 기록할 리스트 생성
+	list->head = NULL;
+	list->tail = NULL;
+
 	//모니터링 시작
 	while (1) {
-
+		list->cur = list->head;
+		mntr_files(mntrDir, fp, list);	
+		usleep(500);
 	}
-
 	exit(0);
 }
 
@@ -64,4 +74,147 @@ int daemon_init(void) //디몬프로세스 생성
 	dup(0);
 	dup(0);
 	return 0;
+}
+
+void mntr_files(char *path, FILE *fp, Llist* list)
+{
+	struct stat statbuf;
+	struct dirent **items;
+	int nitems, i, j;
+
+	nitems = scandir(path, &items, NULL, alphasort); //알파벳 순서로 items에 저장
+
+	for (i = 0; i < nitems; i++) { //하위 파일 출력
+		char childPath[PATHLEN];
+
+		if ((!strcmp(items[i]->d_name, ".")) || (!strcmp(items[i]->d_name, ".."))) //현재, 부모디렉토리 제외
+			continue;
+
+		strcpy(childPath, path); strcat(childPath, "/"); strcat(childPath, items[i]->d_name);
+		lstat(childPath, &statbuf);
+
+		if (list->cur == NULL) //cur == NULL이면 childPath추가
+			add_list(fp, list, childPath);	
+		else if (strcmp(list->cur->file_name, childPath) == 0) {//리스트에 이미 있으면 수정시간만 확인 
+			if (list->cur->mtime != statbuf.st_mtime) { //수정시간이 다를 경우 
+				list->cur->mtime = statbuf.st_mtime; //수정 시간 변경 
+				write_log(fp, list->cur, MODIFY); //로그 기록 
+				list->cur->mtime = statbuf.st_mtime; //리스트 갱신 
+			}
+		}
+		else { //리스트에 없을 때
+			if (search_data(list, childPath))  //list->cur삭제, childPath까지
+				delete_until_speciData(fp, list, childPath); //삭제 및 삭제 로그 기록
+			else { //childPath추가
+				add_list(fp, list, childPath);
+				list->cur->mtime = statbuf.st_mtime; //리스트 수정시간 갱신
+			}
+		}
+
+		list->cur = list->cur->next; //cur이동
+		if (S_ISDIR(statbuf.st_mode)) //디렉토리면 재귀
+			mntr_files(childPath, fp, list);  
+	}  
+}
+
+void write_log(FILE *fp, node *file, int status)
+{
+	char buf[BUFLEN]; 
+	char filePath[PATHLEN]; 
+	char *fileName;
+	struct tm *now;
+	int i;
+
+	now = localtime(&file->mtime);
+	strcpy(filePath, file->file_name);
+	fileName = filePath + strlen(filePath) - 1;
+	while (*fileName != '/')
+		--fileName;
+	++fileName; //파일 이름만 가져오기
+
+	switch (status) {
+		case ADD: //추가
+			sprintf(buf, "[%d-%d-%d %d:%d:%d][create_%s]\n", now->tm_year+1900, now->tm_mon+1, now->tm_mday+1, now->tm_hour, now->tm_min, now->tm_sec, fileName);
+			break;
+		case MODIFY: //수정
+			sprintf(buf, "[%d-%d-%d %d:%d:%d][modify_%s]\n", now->tm_year+1900, now->tm_mon+1, now->tm_mday+1, now->tm_hour, now->tm_min, now->tm_sec, fileName);
+			break;
+		case DELETE: //삭제
+			sprintf(buf, "[%d-%d-%d %d:%d:%d][delete_%s]\n", now->tm_year+1900, now->tm_mon+1, now->tm_mday+1, now->tm_hour, now->tm_min, now->tm_sec, fileName);
+			break;
+	}
+
+	fwrite(buf, strlen(buf), 1, fp);  //log.txt에 작성
+}
+
+void delete_until_speciData(FILE *fp, Llist *list, char *fname) 
+{
+	if(list->cur->prev == NULL) { //prev가 없을 때(cur가 head일 때)
+		while (strcmp(list->cur->file_name, fname) != 0) {
+			list->cur->mtime = time(NULL);
+			write_log(fp, list->cur, DELETE); //삭제 로그 기록
+			node *tmp = list->cur;
+			list->cur = list->cur->next;
+			free(tmp);
+		}
+		list->head = list->cur;
+		list->cur->prev = NULL;
+	}
+	else {
+		node *prev_file = list->cur->prev;
+		while (strcmp(prev_file->next->file_name, fname) != 0) {
+			prev_file->next->mtime = time(NULL);
+			write_log(fp, prev_file->next, DELETE); //삭제 로그 기록
+			node *tmp = prev_file->next;
+			prev_file->next->next->prev = prev_file;
+			prev_file->next = prev_file->next->next;
+			free(tmp);
+		}
+		list->cur = prev_file->next; //list->cur == fname 
+	}
+}
+
+void add_list(FILE *fp, Llist* list, char *fname)
+{
+	node *tmp = (node*)malloc(sizeof(node));
+	strcpy(tmp->file_name, fname);
+	tmp->mtime = time(NULL);
+	if(list->head == NULL && list->tail == NULL) {
+		tmp->prev = NULL;
+		tmp->next = NULL;
+		list->head = tmp;
+		list->tail = tmp->next;
+	}
+	else if (list->cur == NULL) { //list->cur가 tail일 경우
+		node *prev_file = list->cur->prev;	
+		prev_file->next = tmp;
+		tmp->prev = prev_file;
+		tmp->next = NULL;
+		list->tail = tmp->next;
+	}
+	else if(list->cur->prev == NULL) { //list->cur가 head일 경우
+		tmp->next = list->cur;
+		tmp->prev = NULL;
+		list->cur->prev = tmp;
+		list->head = tmp;
+	}
+	else { //리스트 중간에 insert
+		node* prev_file = list->cur->prev;
+		prev_file->next = tmp;
+		tmp->prev = prev_file;
+		tmp->next = list->cur;
+		list->cur->prev = tmp;
+	}
+	list->cur = tmp; //list->cur == fname
+	write_log(fp, list->cur, ADD);
+}
+
+int search_data(Llist *l, char *fname) //리스트에 fname이 있으면 true, 없으면 false 리턴
+{
+	node *cur = l->head;
+	while(cur != NULL) {
+		if(strcmp(cur->file_name, fname) == 0) return true;
+		cur = cur->next;
+	}
+	return false;
 }
--- daemon.h
+++ daemon.h
@@ -2,7 +2,35 @@
 #define DAEMON_H_
 
 #define BUFLEN 1024
+#define FILELEN 256 
+#define PATHLEN 512 
+#define true 1
+#define false 0
+#define ADD 2
+#define MODIFY 3
+#define DELETE 4
+
+typedef struct Llist {
+    struct node *head;
+	struct node *cur;
+    struct node *tail;
+} Llist;
+
+typedef struct node {
+    struct node *next;
+	struct node *prev;
+    char file_name[PATHLEN]; //파일이름 저장
+	time_t mtime; //최종 수정시간
+} node;
 
 int daemon_init(void); //디몬 프로세스 생성
+int search_data(Llist *l, char *fname); //현재 리스트에 있으면 true, 없으면 false 리턴
+void mntr_files(char *path, FILE *fp, Llist *list); //재귀를 통해 파일현황 모니터링
+
+void write_log(FILE *fp, struct node *file, int status);
+void delete_until_speciData(FILE *fp, Llist *list, char *fname);
+void add_list(FILE *fp, Llist* list, char *fname);
+int search_data(Llist *l, char *fname); //리스트에 fname이 있으면 true, 없으면 false 리턴
+
 
 #endif
